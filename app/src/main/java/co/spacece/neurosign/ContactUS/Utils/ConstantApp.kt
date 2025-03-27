7c189ca407948e5eacf0cd8bb4c290b712c59eec
waghale <pranotiwaghale@gmail.com>
2025-03-27 15:48:10 +0530

    Update ConstantApp.kt

--- app/src/main/java/co/spacece/neurosign/ContactUS/Utils/ConstantApp.kt
+++ app/src/main/java/co/spacece/neurosign/ContactUS/Utils/ConstantApp.kt
@@ -1,1 +1,10 @@
 "package co.spacece.neurosign.ContactUS.Utils" 
+
+class ConstantApp {
+
+        companion object {
+            const val PHONE_NUMBER = "Mobile Number - +91 9096305648"
+            const val EMAIL = "technology.spacece@gmail.com"
+            const val ADDRESS = "SpacECE, Bhagwat Bungalow, Raikar Nagar, Dhayari Pune 411041"
+        }
+}
