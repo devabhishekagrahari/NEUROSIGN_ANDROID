2397519e2cf49fabfb308135ec19c12b3cc5357f
Masoom08 <masoomsingh0801@gmail.com>
2025-03-28 14:57:08 +0530

    version 1.5 EarlyAssessment Page 1

--- app/src/main/java/co/spacece/neurosign/ui/theme/Color.kt
+++ app/src/main/java/co/spacece/neurosign/ui/theme/Color.kt
@@ -11,10 +11,14 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Primary=Color(0xFFF8A800)
-val Light=Color(0xFFCCCCCC)
+//val Light=Color(0xFFCCCCCC)
 
 
 val AppPrimary = Color(0xFFF8A800)
 val AppAccent = Color(0xFF85D5EF)
 val DarkText = Color(0xFF333333)
 val LightGray = Color(0xFFEEEEEE)
+val Background = Color(0xFFEEEEEE)
+val Dark = Color(0xFF333333)
+val Medium = Color(0xFF666666)
+val Light = Color(0xCCCCCCCC)
